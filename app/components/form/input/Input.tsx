d0d2984351e649c9e36a30306ac9b05b732c1f7d
AliDevBla <ali.dev@blaagency.com>
2023-12-04 23:01:35 +0500

    Add input simple and newsletter variant

--- app/components/form/input/Input.tsx
+++ app/components/form/input/Input.tsx
@@ -1,7 +1,46 @@
+"use client";
 import React from "react";
+import styles from "./Input.module.css";
+import { ReactSVG } from "react-svg";
 
-const Input = () => {
-  return <div>Input</div>;
+interface InputProps {
+  placeholder: string;
+  type: "text" | "email" | "password" | "tel";
+  variant: "simple" | "newsletter";
+}
+
+const Input = (props: InputProps) => {
+  if (props.variant === "simple") {
+    return (
+      <input
+        className={styles.simple_input}
+        type={props.type}
+        placeholder={props.placeholder}
+      />
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        id="email"
+        placeholder="Email"
+      />
+      {props.variant === "newsletter" && (
+        <div className={styles.iconWrapper}>
+          <ReactSVG
+            className={styles.icon}
+            src="/images/icons/right-arrow-white.svg"
+            width={20}
+            height={20}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Input;
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import Button from "../components/ui/button/Button";
+import Input from "../components/form/input/Input";
+// import { ReactSVG } from "react-svg";
 
 const Playground = () => {
   return (
-    <div>
-      <Button />
+    <div className=" h-screen flex flex-col gap-4  p-10 max-w-5xl mx-auto">
+      <Input type="text" placeholder="Nombre" variant="simple" />
+      <Input type="text" placeholder="Apellidos" variant="simple" />
+      <Input type="email" placeholder="Email" variant="simple" />
+      <Input type="password" placeholder="Contraseña" variant="simple" />
+      <Input type="text" placeholder="Fecha de nacimiento" variant="simple" />
+      <Input type="text" placeholder="Nacionalidad" variant="simple" />
+      <Input type="tel" placeholder="+1 456 234 7890" variant="simple" />
+      <div className="flex gap-10">
+        <Input type="text" placeholder="DNI" variant="simple" />
+        <Input type="text" placeholder="Aquí el número…" variant="simple" />
+      </div>
+      <Input type="text" placeholder="Género" variant="simple" />
+      <Input
+        type="text"
+        placeholder="¿Tienes diagnóstico TEA confirmado?"
+        variant="simple"
+      />
+      <div className="flex gap-10">
+        <div className="flex-[3]">
+          <Input
+            type="text"
+            placeholder="¿Tienes certificado de discapacidad?"
+            variant="simple"
+          />
+        </div>
+        <div className="flex-[1]">
+          <Input type="text" placeholder="26%" variant="simple" />
+        </div>
+      </div>
+      <div className="mt-10 bg-black p-10">
+        <Input type="email" placeholder="Contraseña" variant="newsletter" />
+      </div>
+      {/* <ReactSVG src="/images/" /> */}
     </div>
   );
 };
