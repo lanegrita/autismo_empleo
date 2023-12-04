6822d2f1fcff9b226c02d9cd0abb71356fc72038
shayanshoukatbla <shayan.shoukat@blaagency.com>
2023-12-04 23:00:14 +0500

    Add first and second section buttons variants in Button component

--- app/components/ui/button/Button.tsx
+++ app/components/ui/button/Button.tsx
@@ -1,7 +1,63 @@
+"use client";
 import React from "react";
+import styles from "./Button.module.css";
+import Image from "next/image";
+import { ReactSVG } from "react-svg";
 
-const Button = () => {
-  return <div>Button</div>;
+interface ButtonProps {
+  title?: string;
+  onClick?: () => void;
+  variant:
+    | "primary"
+    | "secondary"
+    | "outline"
+    | "simple"
+    | "primary-azul"
+    | "secondary-azul"
+    | "outline-azul"
+    | "simple-azul";
+  icon?: string;
+  direction?: "left" | "right";
+  iconSize?: number;
+}
+
+const Button = ({
+  title,
+  onClick,
+  variant,
+  icon = "",
+  direction = "left",
+  iconSize = 24,
+}: ButtonProps) => {
+  return (
+    <button
+      className={[
+        styles.container,
+        variant === "primary" && styles.primary,
+        variant === "secondary" && styles.secondary,
+        variant === "outline" && styles.outline,
+        variant === "simple" && styles.simple,
+        // Azol Styles Start
+        variant === "primary-azul" && styles.primaryAzul,
+        variant === "secondary-azul" && styles.secondaryAzul,
+        variant === "outline-azul" && styles.outlineAzul,
+        variant === "simple-azul" && styles.simpleAzul,
+        // Azol Styles Ends
+        direction === "left" && styles.left,
+        direction === "right" && styles.right,
+        icon && title && styles.withIcon,
+        icon && !title && styles.onlyIcon,
+      ].join(" ")}
+    >
+      {title && title}
+      {/* Icon Image */}
+      {icon && (
+        <div className={styles.iconWrapper}>
+          <ReactSVG className={styles.iconImage} src={icon} />
+        </div>
+      )}
+    </button>
+  );
 };
 
 export default Button;
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,10 +1,148 @@
 import React from "react";
 import Button from "../components/ui/button/Button";
 
+const rightArrow = "/images/icons/right-arrow-white.svg";
+const cartArrow = "/images/icons/cart-icon.svg";
+const pencilIcon = "/images/icons/pencil-outline.svg";
+const downloadIcon = "/images/icons/download-icon.svg";
 const Playground = () => {
   return (
     <div>
-      <Button />
+      <div className="px-5 py-10 grid grid-cols-4 gap-36 container mt-8 ">
+        <Button title="Continue" variant="primary" />
+        <Button title="Continue" variant="secondary" />
+        <Button title="Continue" variant="outline" />
+        <Button title="Continue" variant="simple" />
+        {/* Left Direction Buttons */}
+        <Button
+          title="Inscríbete"
+          variant="primary"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Inscríbete"
+          variant="secondary"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Continue"
+          variant="outline"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Continue"
+          variant="simple"
+          icon={downloadIcon}
+          direction="left"
+        />
+        {/* Right Direction Buttons */}
+        <Button
+          title="Inscríbete"
+          variant="primary"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Inscríbete"
+          variant="secondary"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Continue"
+          variant="outline"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Continue"
+          variant="simple"
+          icon={downloadIcon}
+          direction="right"
+        />
+
+        {/* Without Text Buttons */}
+        <Button variant="primary" icon={pencilIcon} direction="right" />
+        <Button variant="secondary" icon={pencilIcon} direction="right" />
+        <Button variant="outline" icon={pencilIcon} direction="right" />
+        <Button
+          iconSize={20}
+          variant="simple"
+          icon={downloadIcon}
+          direction="right"
+        />
+      </div>
+      {/* Second Section */}
+      <div className="px-5 py-10 grid grid-cols-4 gap-36 container mt-8 bg-blue-500 ">
+        <Button title="Continue" variant="primary-azul" />
+        <Button title="Continue" variant="secondary-azul" />
+        <Button title="Continue" variant="outline-azul" />
+        <Button title="Continue" variant="simple-azul" />
+        {/* Left Direction Buttons */}
+        <Button
+          title="Inscríbete"
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Inscríbete"
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Continue"
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Continue"
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="left"
+        />
+        {/* Right Direction Buttons */}
+        <Button
+          title="Inscríbete"
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Inscríbete"
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Continue"
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Continue"
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="right"
+        />
+
+        {/* Without Text Buttons */}
+        <Button variant="primary-azul" icon={pencilIcon} direction="right" />
+        <Button variant="secondary-azul" icon={pencilIcon} direction="right" />
+        <Button variant="outline-azul" icon={pencilIcon} direction="right" />
+        <Button
+          iconSize={20}
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="right"
+        />
+      </div>
     </div>
   );
 };
