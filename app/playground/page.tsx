24b0a0ae061a0126e9daf7f5125b691f194d1630
shayanshoukatbla <shayan.shoukat@blaagency.com>
2023-12-05 20:05:33 +0500

    Add disable buttons of the azul buttons  section

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../components/ui/button/Button";
 
 const rightArrow = "arrow-right.svg";
-const cartIcon = "/images/icons/cart-icon.svg";
+const newsIcon = "/images/icons/news.svg";
 const pencilIcon = "/images/icons/pencil.svg";
 const downloadIcon = "/images/icons/download.svg";
 const Playground = () => {
@@ -83,77 +83,7 @@ const Playground = () => {
           direction="right"
         />
       </div>
-      <h1 className="text-5xl my-6">Azul buttons</h1>
       {/* Second Section */}
-
-      <div className="px-5 py-10 grid grid-cols-4 gap-36 mt-8 bg-blue-500 ">
-        <Button title="Continue" variant="primary-azul" />
-        <Button title="Continue" variant="secondary-azul" />
-        <Button title="Continue" variant="outline-azul" />
-        <Button title="Continue" variant="simple-azul" />
-        {/* Left Direction Buttons */}
-        <Button
-          title="Inscríbete"
-          variant="primary-azul"
-          icon={pencilIcon}
-          direction="left"
-        />
-        <Button
-          title="Inscríbete"
-          variant="secondary-azul"
-          icon={pencilIcon}
-          direction="left"
-        />
-        <Button
-          title="Continue"
-          variant="outline-azul"
-          icon={pencilIcon}
-          direction="left"
-        />
-        <Button
-          title="Continue"
-          variant="simple-azul"
-          icon={downloadIcon}
-          direction="left"
-        />
-        {/* Right Direction Buttons */}
-        <Button
-          title="Inscríbete"
-          variant="primary-azul"
-          icon={pencilIcon}
-          direction="right"
-        />
-        <Button
-          title="Inscríbete"
-          variant="secondary-azul"
-          icon={pencilIcon}
-          direction="right"
-        />
-        <Button
-          title="Continue"
-          variant="outline-azul"
-          icon={pencilIcon}
-          direction="right"
-        />
-        <Button
-          title="Continue"
-          variant="simple-azul"
-          icon={downloadIcon}
-          direction="right"
-        />
-
-        {/* Without Text Buttons */}
-        <Button variant="primary-azul" icon={pencilIcon} direction="right" />
-        <Button variant="secondary-azul" icon={pencilIcon} direction="right" />
-        <Button variant="outline-azul" icon={pencilIcon} direction="right" />
-        <Button
-          iconSize={20}
-          variant="simple-azul"
-          icon={downloadIcon}
-          direction="right"
-        />
-      </div>
-      {/* Third Section */}
       <h1 className="text-5xl my-6">Default disabled buttons</h1>
       <div className="px-5 py-10 grid grid-cols-4 gap-36 mt-8 mb-12 ">
         <Button disabled title="Continue" variant="primary" size="xl" />
@@ -230,6 +160,171 @@ const Playground = () => {
           direction="right"
         />
         <Button disabled variant="simple" icon={downloadIcon} />
+      </div>
+      {/* Third Section */}
+      <h1 className="text-5xl my-6">Azul buttons</h1>
+
+      <div className="px-5 py-10 grid grid-cols-4 gap-36 mt-8 bg-[#1a9eda] ">
+        <Button title="Continue" variant="primary-azul" />
+        <Button title="Continue" variant="secondary-azul" />
+        <Button title="Continue" variant="outline-azul" />
+        <Button title="Continue" variant="simple-azul" />
+        {/* Left Direction Buttons */}
+        <Button
+          title="Inscríbete"
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Inscríbete"
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Continue"
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          title="Continue"
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="left"
+        />
+        {/* Right Direction Buttons */}
+        <Button
+          title="Inscríbete"
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Inscríbete"
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Continue"
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          title="Continue"
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="right"
+        />
+
+        {/* Without Text Buttons */}
+        <Button variant="primary-azul" icon={pencilIcon} direction="right" />
+        <Button variant="secondary-azul" icon={pencilIcon} direction="right" />
+        <Button variant="outline-azul" icon={pencilIcon} direction="right" />
+        <Button
+          iconSize={20}
+          variant="simple-azul"
+          icon={newsIcon}
+          direction="right"
+        />
+      </div>
+
+      {/* Disabled Azul Buttons */}
+      <h1 className="text-5xl my-6">Disabled Azul buttons</h1>
+
+      <div className="px-5 py-10 grid grid-cols-4 gap-36 mt-8 bg-[#1a9eda] ">
+        <Button disabled title="Continue" variant="primary-azul" />
+        <Button disabled title="Continue" variant="secondary-azul" />
+        <Button disabled title="Continue" variant="outline-azul" />
+        <Button disabled title="Continue" variant="simple-azul" />
+        {/* Left Direction Buttons */}
+        <Button
+          disabled
+          title="Inscríbete"
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          disabled
+          title="Inscríbete"
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          disabled
+          title="Continue"
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="left"
+        />
+        <Button
+          disabled
+          title="Continue"
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="left"
+        />
+        {/* Right Direction Buttons */}
+        <Button
+          disabled
+          title="Inscríbete"
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          disabled
+          title="Inscríbete"
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          disabled
+          title="Continue"
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          disabled
+          title="Continue"
+          variant="simple-azul"
+          icon={downloadIcon}
+          direction="right"
+        />
+
+        {/* Without Text Buttons */}
+        <Button
+          disabled
+          variant="primary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          disabled
+          variant="secondary-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          disabled
+          variant="outline-azul"
+          icon={pencilIcon}
+          direction="right"
+        />
+        <Button
+          disabled
+          iconSize={20}
+          variant="simple-azul"
+          icon={newsIcon}
+          direction="right"
+        />
       </div>
     </div>
   );
